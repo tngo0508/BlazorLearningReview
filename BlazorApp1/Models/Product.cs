04f94ce1d45279236aa95495ca0371c464ee1a17
Thomas Ngo <tngo@ochca.com>
2024-10-09 15:53:36 -0700

    Add Product model and bind it in BindProp component
    Integrated a new Product model to encapsulate product properties such as Id, Name, Price, and IsActive. Updated the BindProp component to display and bind product data, allowing real-time updates of product price through input binding.

---
+++ BlazorApp1/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp1.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public bool IsActive { get; set; }
+}
